c57098f470b712dc573a5b4f25b14ddba0dc902b
Adam <leiadam74@gmail.com>
2022-09-07 02:00:24 -0400

    Added textbox, social icons, and svgs

--- src/components/ContentComp/Content.js
+++ src/components/ContentComp/Content.js
@@ -1,19 +1,31 @@
 import React from "react";
+import insta from "../../assets/logo-instagram.svg";
+import twitter from '../../assets/logo-twitter_3.svg';
+import discord from '../../assets/logo-discord.svg';
+import facebook from '../../assets/logo-facebook_1.svg';
 
 export default function ContentBox(props) {
   return (
-  <div className="bg-slate-700 text-white rounded">{/*rounded box*/}
+  <div className="bg-white h-5/6 w-2/3 object-center mx-auto self-center rounded-[36px] p-10">{/*rounded box*/}
     <div>{/* logo*/}
         <img></img>
     </div>
-    <div className="overflow-y-auto">{/* text box*/}
+    <div className="overflow-y-auto h-[675px]">{/* text box*/}
     {props.children}
     </div>
-    <div>{/* footer social icons*/} 
-        <img></img>
-        <img></img>
-        <img></img>
-        <img></img>
+    <div className='flex flex-row justify-center pt-3'>{/* footer social icons*/} 
+        <div className="px-6">
+          <img src={insta} className='h-12 w-12 flex-initial'></img>
+        </div>
+        <div className="px-6">
+          <img src={twitter} className='h-12 w-12 flex-initial'></img>
+        </div>
+        <div className="px-6">
+          <img src={facebook} className='h-12 w-12 flex-initial'></img>
+        </div>
+        <div className="px-6">
+          <img src={discord} className='h-12 w-12 flex-initial'></img>
+        </div>
     </div>
   </div>
   );
