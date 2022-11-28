9db67949abe0c827ceb2b35ccc17cd25b8e08175
Mark Bruckert <markbruckert1@Gmail.com>
2022-11-28 11:53:45 -0500

    Fixing Merge Conflicts

--- src/components/ContentBox/ContentBox.js
+++ src/components/ContentBox/ContentBox.js
@@ -1,6 +1,5 @@
 import React from "react";
 import insta from "../../assets/logo-instagram.svg";
-<<<<<<< HEAD
 import twitter from "../../assets/logo-twitter_3.svg";
 import discord from "../../assets/logo-discord.svg";
 import facebook from "../../assets/logo-facebook_1.svg";
@@ -46,35 +45,3 @@ export default function ContentBox(props) {
   );
 }
 
-=======
-import twitter from '../../assets/logo-twitter_3.svg';
-import discord from '../../assets/logo-discord.svg';
-import facebook from '../../assets/logo-facebook_1.svg';
-
-export default function ContentBox(props) {
-  return (
-  <div className="bg-white h-5/6 w-1/2 object-center mx-auto self-center rounded-[36px] p-10">{/*rounded box*/}
-    <div>{/* logo*/}
-        <img></img>
-    </div>
-    <div className="overflow-y-auto h-5/6">{/* text box*/}
-    {props.children}
-    </div>
-    <div className='flex flex-row justify-center pt-20 pt-3'>{/* footer social icons*/} 
-        <div className="px-6">
-          <button><img src={insta} className='h-12 w-12 flex-initial'></img></button>
-        </div>
-        <div className="px-6">
-          <button><img src={twitter} className='h-12 w-12 flex-initial'></img></button>
-        </div>
-        <div className="px-6">
-          <button><img src={facebook} className='h-12 w-12 flex-initial'></img></button>
-        </div>
-        <div className="px-6">
-          <button><img src={discord} className='h-12 w-12 flex-initial'></img></button>
-        </div>
-    </div>
-  </div>
-  );
-}
->>>>>>> c99d03cbaa96b58af6d07bcb1d3d6d7cd92fcdf7
